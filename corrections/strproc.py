c7bf92875d545f606029e919712d915397319e58
Loïc Grobol <loic.grobol@gmail.com>
2019-11-06 09:43:50 +0100

    strproc correction

--- corrections/strproc.py
+++ corrections/strproc.py
@@ -1,10 +1,24 @@
+import re
 import sys
 
 
 def main(inpt):
     with open(inpt) as in_stream:
         for l in in_stream:
-            print(l.strip())
+            l = l.strip("\n")
+            if "spam" in l:
+                print("a")
+            elif re.match(r".{,4}z$", l):
+                print("b")
+            elif re.search(r"(\w+\.\w+@\w+\.\w+)", l):
+                match = re.search(r"(\w+\.\w+@\w+\.\w+)", l)
+                print(match.group(1))
+            elif l[:3] == l[-3:]:
+                print("d")
+            elif l == l[::-1]:
+                print("e")
+            else:
+                print("NOPE")
 
 
 if __name__ == "__main__":
